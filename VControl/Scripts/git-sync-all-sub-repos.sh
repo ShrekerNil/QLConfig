7e8ca4e913aa0f3f933db467560526b5f40277bd
Shreker.Nil <shrekernil@qq.com>
2024-11-17 02:02:10 +0800

    -- Auto Save Files --

--- VControl/Scripts/git-sync-all-sub-repos.sh
+++ VControl/Scripts/git-sync-all-sub-repos.sh
@@ -2,6 +2,13 @@
 
 exec > >(tee "${CUR_DIR}/sync.log") 2>&1
 
+print_call_stack() {
+    echo "调用链："
+    for i in $(seq 0 $((${#BASH_SOURCE[@]} - 1))); do
+        echo "  $i: ${BASH_SOURCE[$i]}:${BASH_LINENO[$i]}"
+    done
+}
+
 function echo_sucess() {
     echo -e "GIT-SYNC-CTRL:" "\033[32m$1\033[0m"
 }
@@ -72,7 +79,7 @@ function start_process() {
                     echo_info "Synchronizing AUTO Git Repo: ${sub_dir}"
                     new_line
                     # bash /d/QLRepo/QLNotes/Config/VControl/Scripts/git-sync.sh `pwd`
-                    bash ${sub_dir}/git-sync.sh `pwd`
+                    bash ${sub_dir}/git-sync.sh `pwd` print_call_stack
                     continue
                 else
                     new_line
